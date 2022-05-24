c1bd4a52439cb9161fe81923720ce07ab3d7393b
Sean Cabahug <25161923+seancabahug@users.noreply.github.com>
2022-05-24 12:54:39 -0700

    Initial commit

---
+++ .config/polybar/dummybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/dummybar"
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 Dotfiles to replicate my Linux GUI environment
+
+## Quick Install (with paru)
+```
+paru -S i3-gaps-rounded-git i3lock rofi polybar rxvt-unicode
+```
